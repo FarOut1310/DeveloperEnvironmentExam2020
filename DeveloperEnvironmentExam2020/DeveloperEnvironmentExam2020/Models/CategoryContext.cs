acf3417df43410090e6ec803688f13b29d6d0a3a
FAROUTPC\Sean <sean.rcordero@gmail.com>
2020-06-18 09:30:22 +0200

    Added NuGet packages for EntityFrameworkCore. Added context classes for models.

---
+++ DeveloperEnvironmentExam2020/DeveloperEnvironmentExam2020/Models/CategoryContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeveloperEnvironmentExam2020.Models
+{
+    public class CategoryContext : DbContext
+    {
+        public CategoryContext(DbContextOptions<CategoryContext> options) : base(options) { }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ DeveloperEnvironmentExam2020/DeveloperEnvironmentExam2020/Models/ProductContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeveloperEnvironmentExam2020.Models
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext(DbContextOptions<ProductContext> options) : base(options) { }
+        public DbSet<Product> Products { get; set; }
+    }
+}
