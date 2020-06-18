bfc1cb0fde84b31aded74bd450787d9e82e6e7b7
FAROUTPC\Sean <sean.rcordero@gmail.com>
2020-06-18 09:18:11 +0200

    First Commit. New ASP.NET Core Web Application project for building API. Added model classes for assignment.

---
+++ DeveloperEnvironmentExam2020/DeveloperEnvironmentExam2020/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeveloperEnvironmentExam2020.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Unit { get; set; }
+        public int Amount { get; set; }
+        public double Price { get; set; }
+
+        public Category Category { get; set; }
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
